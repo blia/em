b445c10b1e95530d6d13ac3ac09b154464e0e3a3
Kirill Yakovenko <kirill.yakovenko@me.com>
2014-10-23 20:18:22 +0300

    es6-modules

--- tasks/es6-modules.js
+++ tasks/es6-modules.js
@@ -2,19 +2,20 @@ var log = require('../lib/logger')
   , transpiler = require('es6-module-transpiler')
   , Container = transpiler.Container
   , FileResolver = transpiler.FileResolver
-  , BundleFormatter = transpiler.formatters.bundle;
-
+  , BundleFormatter = transpiler.formatters.bundle
 
 module.exports = function () {
-  // var container = new Container({
-  //   resolvers: [new FileResolver(['./'])],
-  //   formatter: new BundleFormatter()
-  // })
-  // try {
-  //   container.getModule('app/app.js')
-  //   container.write('build/es5.js')
-  // } catch (err) {
-  //   log.error('ES6 '+err.toString())
-  // }
-
+  return function (fn) {
+    var container = new Container({
+      resolvers: [new FileResolver(['./'])],
+      formatter: new BundleFormatter()
+    })
+    try {
+      container.getModule('app/app.js')
+      container.write('build/es5.js')
+      return fn(null, 'Es6 modules: buil success')
+    } catch (err) {
+      return fn('Es6 modules: ' + err.toString())
+    }
+  }
 }
